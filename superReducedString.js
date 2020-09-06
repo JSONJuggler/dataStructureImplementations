1c9b10b13b80e8e8fb93b930096549ee316aa895
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-09-06 15:33:55 -0500

    superReducedString using recursion

---
+++ superReducedString.js
@@ -0,0 +1,50 @@
+const superReducedString = (s) => {
+  let result;
+
+  const dupeCheck = (s) => {
+    let idx = 0;
+    let checker = 1;
+    let duplicates = "";
+    let dupeIdxArr = [];
+    let string = "";
+
+    while (checker <= s.length) {
+      if (s[idx] === s[checker]) {
+        duplicates = duplicates + s.slice(idx, checker + 1);
+        dupeIdxArr.push(idx);
+        dupeIdxArr.push(checker);
+        idx++;
+        idx++;
+        checker++;
+        checker++;
+      } else {
+        idx++;
+        checker++;
+      }
+    }
+
+    if (duplicates) {
+      for (let i = 0; i < s.length; i++) {
+        if (i === dupeIdxArr[0]) {
+          dupeIdxArr = dupeIdxArr.slice(1);
+        } else {
+          string = string + s[i];
+        }
+      }
+
+      return dupeCheck(string);
+    } else {
+      return s;
+    }
+  };
+
+  result = dupeCheck(s);
+
+  if (result.length === 0) {
+    //console.log("Empty String");
+    return "Empty String";
+  } else {
+    //console.log(result);
+    return result;
+  }
+};
